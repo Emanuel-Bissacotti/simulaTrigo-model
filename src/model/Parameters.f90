23e6f714288d6aa32468c11477d0a47f07d1fd52
Emanuel-Bissacotti <emanuelfbissacotti@gmail.com>
2025-03-18 18:44:11 +0000

    Adiciona Parameters.f90
    * Adiciona parametros do modelo: sowing_year, sowing_date e type_cultivar.

---
+++ src/model/Parameters.f90
@@ -0,0 +1,8 @@
+module parameters
+    implicit none
+    integer, public :: parameters_len = 3
+    integer, public :: sowing_year
+    integer, public :: sowing_date
+    integer, public :: type_cultivar
+
+end module parameters
